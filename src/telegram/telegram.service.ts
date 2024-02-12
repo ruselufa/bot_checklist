82e1ab3c9985003f1440f463c5b561d6f4ccd79c
Ruslan Galiev <ghaliev@gmail.com>
2024-02-12 14:16:01 +0000

    добавлены доп сообщения

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { error } from 'console';
 import { ErrorLoggerService } from 'src/error-logger/error-logger.service';
 import {
 	filesArray1,
@@ -20,34 +21,65 @@ export class TelegramService {
 		this.bot = new Telegraf('6594921942:AAGkRhlqIsyEtCjXtOh-73BNVvzB_SOBQSI');
 
 		this.bot.start(async (ctx) => {
-			await ctx.reply(
-				'Привет! Добро пожаловать!',
+			const username = ctx.message.from.first_name;
+			ctx.reply(
+				`Поздравляем, ${username}!
+
+Вы успешно подключили бота!
+
+В 4 уроке мы приготовили для вас много подарков!
+
+Поэтому дойдите до 4 урока, чтобы забрать ваши бонусы. До скорой встречи, буду ждать вас здесь:)`,
 				Markup.inlineKeyboard([
-					Markup.button.callback('Получить бонусы', 'getbonus1'),
-					// Markup.button.callback('Получить бонус2', 'getbonus2'),
-				]),
+					Markup.button.url('Вернуться к 1 уроку', 'https://online.azat-valeev.ru/course-practic-av-lesson-01'),
+				])
 			);
 		});
 
-		this.bot.action('getbonus1', async (ctx) => {
-			try {
-				await ctx.reply('Оформляю бонусы');
-				await delay(2000);
-				await ctx.reply('Загружаю бонусы в сеть');
-				await delay(2000);
-				await ctx.reply('Начинаю отправку');
-				await sendMediaGroup(ctx, filesArray1);
-				await sendMediaGroup(ctx, filesArray2);
-				await sendMediaGroup(ctx, filesArray3);
-				await sendMediaGroup(ctx, filesArray4);
-				await sendMediaGroup(ctx, filesArray5);
-				await sendMediaGroup(ctx, filesArray6);
-				await ctx.reply('Все отправлено');
-			} catch (error) {
-				console.error('Ошибка при выполнении запроса:', error);
-				this.errorLogger.logError(error);
-				return ctx.reply('Произошла ошибка при отправке. Пожалуйста, попробуйте снова.');
-			}
+		this.bot.hears(/халвинг/i, async (ctx) => {
+			setTimeout(async () => {
+				try {
+					await ctx.reply('Как и договаривались! Высылаем вам дополнительные материалы к уроку ценностью 10 000 руб.');
+					await delay(2000);
+					await ctx.reply('Оформляем бонусы');
+					await delay(2000);
+					await ctx.reply('Загружаем бонусы в сеть');
+					await delay(2000);
+					await ctx.reply('Начинаем отправку');
+	
+					const promises = [
+						sendMediaGroup(ctx, filesArray1).catch(error => { throw error; }),
+						sendMediaGroup(ctx, filesArray2).catch(error => { throw error; }),
+						sendMediaGroup(ctx, filesArray3).catch(error => { throw error; }),
+						sendMediaGroup(ctx, filesArray4).catch(error => { throw error; }),
+						sendMediaGroup(ctx, filesArray5).catch(error => { throw error; }),
+						sendMediaGroup(ctx, filesArray6).catch(error => { throw error; }),
+					];
+	
+					const results = await Promise.allSettled(promises);
+					results.forEach(result => {
+						if (result.status === 'rejected') {
+							console.error('Ошибка при выполнении запроса: ', error);
+							this.errorLogger.logError(result.reason);
+						}
+					})
+					await delay(2000);
+					await ctx.reply(`Ойй...Что мы вам приготовили в 5 уроке!
+	
+	Этот тот самый подарок, который доступен всего 24 часа! Быстрее переходите к следующему уроку!
+	`,
+						Markup.inlineKeyboard([
+							Markup.button.url('Вернуться к 4 уроку', 'https://online.azat-valeev.ru/course-practic-av-lesson-04'),
+						])
+					);
+	
+	
+				} catch (error) {
+					console.error('Ошибка при выполнении запроса:', error);
+					this.errorLogger.logError(error);
+					return ctx.reply('Произошла ошибка при отправке. Пожалуйста, попробуйте снова.');
+				}
+			}, 0);
 		});
 
 		// Функция для задержки
